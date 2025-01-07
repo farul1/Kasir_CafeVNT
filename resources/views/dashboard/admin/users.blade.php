25ba247c0974f1439554eb079ed7ca5cb6945eef
einjel21 <151627357+einjel21@users.noreply.github.com>
2025-01-07 16:19:28 +0700

    Update users.blade.php

--- resources/views/dashboard/admin/users.blade.php
+++ resources/views/dashboard/admin/users.blade.php
@@ -1,92 +1,61 @@
-@extends('dashboard.layout.master')
-
-@section('page-dashboard')
-
 <div class="row">
     <div class="col-md-12">
         <div class="white-box">
-            <h3 class="box-title">Admin Dashboard</h3>
-            <div class="mb-3">
-                <a href="/dashboard/users/create" class="btn btn-success text-white shadow-none"><i class="fa-solid fa-circle-plus"></i>&nbsp;&nbsp;Tambah User</a>
-                <a href="/users/export/excel" class="btn btn-info text-white shadow-none ms-2"><i class="fa-solid fa-table"></i>&nbsp;&nbsp;Export (Excel)</a>
-                <a href="/users/export/pdf" class="btn btn-danger text-white shadow-none ms-2"><i class="fa-solid fa-file-pdf"></i>&nbsp;&nbsp;Export (PDF)</a>
-            </div>
-            <form>
-                <label class="form-label">Filter Data</label>
-                <div class="mb-3">
-                    <input type="text" class="form-control pencarian" name="pencarian" placeholder="Pencarian" autocomplete="off">
+            <form action="/dashboard/users" method="post">
+                @csrf
+                <h3 class="box-title">Tambah User</h3>
+                <div class="profile-wrapper mb-2">
+                    <label class="form-label mb-1" for="nama">Nama</label>
+                    <input type="text" class="form-control" name="nama" id="nama" wire:model="nama" placeholder="Masukkan Nama" autocomplete="off">
                 </div>
-                <button type="submit" class="btn btn-secondary text-white shadow-none" type="button" id="button-addon2"><i class="fa-solid fa-magnifying-glass"></i>&nbsp;&nbsp;Cari</button>
+                @error('nama')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <div class="profile-wrapper mb-2">
+                    <label class="form-label mb-1" for="username">Username</label>
+                    <input type="text" class="form-control" name="username" id="username" wire:model="username" placeholder="Masukkan Username" autocomplete="off">
+                </div>
+                @error('username')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <label class="form-label mb-1" for="username">Role</label>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="role" id="admin" value="Admin">
+                    <label class="form-check-label" for="admin">Admin</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="role" id="manager" value="Manager">
+                    <label class="form-check-label" for="manager">Manager</label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="role" id="cashier" value="Cashier">
+                    <label class="form-check-label" for="cashier">Cashier</label>
+                </div>
+                @error('role')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <div class="profile-wrapper mb-2">
+                    <label class="form-label mb-1" for="password">Password</label>
+                    <input type="password" class="form-control" name="password" id="password" wire:model="password" placeholder="Masukkan Password">
+                </div>
+                @error('password')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <div class="profile-wrapper mb-2">
+                    <input class="form-check-input" type="checkbox" id="show-password" onclick="showPasswordHandler()">
+                    <label class="form-check-label" for="show-password">Tampilkan Password</label>
+                </div>
+                <button class="btn btn-success text-white shadow-none mt-2">Tambahkan</button>
+                <a href="/dashboard/users" class="btn btn-danger text-white shadow-none mt-2 ms-2">Kembali</a>
             </form>
         </div>
     </div>
 </div>
-
-<div class="row">
-    <div class="col-sm-12">
-        <div class="white-box">
-            <h3 class="box-title">Tabel Users</h3>
-            <div class="table-responsive">
-                @if(session()->has('success'))
-                    <div class="alert alert-success">
-                        <i class="fa-solid fa-circle-check"></i>&nbsp;&nbsp;{{ session('success') }}
-                    </div>
-                @endif
-                <table class="table text-nowrap">
-                    <thead>
-                        <tr>
-                            <th class="border-top-0">No</th>
-                            <th class="border-top-0">Nama</th>
-                            <th class="border-top-0">Username</th>
-                            <th class="border-top-0">Role</th>
-                            <th class="border-top-0">Tanggal Dibuat</th>
-                            <th class="border-top-0 text-white">#</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($users as $key => $user)
-                            <tr>
-                                <td>{{ $users->firstItem() + $key }}</td>
-                                <td>{{ $user->nama }}</td>
-                                <td>{{ $user->username }}</td>
-                                <td>{{ $user->role }}</td>
-                                <td>{{ $user->created_at }}</td>
-                                <td>
-                                    <a href="/dashboard/users/{{ $user->id }}/edit" class="btn btn-primary shadow-none">Edit</a>
-                                    <a href="#" class="btn btn-danger text-white shadow-none" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $user->id }}">Hapus</a>
-                                    <form action="/dashboard/users/{{ $user->id }}" method="post">
-                                        @csrf
-                                        @method('delete')
-                                        <div class="modal fade" id="deleteModal{{ $user->id }}" tabindex="-1" aria-hidden="true">
-                                            <div class="modal-dialog modal-dialog-centered">
-                                              <div class="modal-content">
-                                                <div class="modal-header">
-                                                  <h5 class="modal-title" id="exampleModalLabel">Delete</h5>
-                                                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                  <label class="mb-2">Apakah anda yakin ingin menghapus user berikut ?</label>
-                                                  <p class="m-0"><b>Nama</b> {{ $user->nama }}</p>
-                                                  <p class="m-0"><b>Username</b> {{ $user->username }}</p>
-                                                  <p class="m-0"><b>Role</b> {{ $user->role }}</p>
-                                                </div>
-                                                <div class="modal-footer">
-                                                  <button type="button" class="btn btn-secondary text-white shadow-none" data-bs-dismiss="modal">Tidak</button>
-                                                  <button type="submit" class="btn btn-danger text-white shadow-none">Hapus</button>
-                                                </div>
-                                              </div>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-    {{ $users->onEachSide(0)->links() }}
-</div>
-
-@endsection
