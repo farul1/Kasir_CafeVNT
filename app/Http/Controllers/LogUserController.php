5ff5ba7e26c22928a92fe7b9dc941f3880297866
Audi87 <58014124+Audi87@users.noreply.github.com>
2025-01-07 16:27:00 +0700

    Update LogUserController.php

--- app/Http/Controllers/LogUserController.php
+++ app/Http/Controllers/LogUserController.php
@@ -7,4 +7,19 @@ use Illuminate\Http\Request;
 
 class LogUserController extends Controller
 {
+    public function index(Request $request)
+    {
+        $logs = LogUser::latest()->paginate(10);
+
+        if($request['date1'] || $request['date2']) {
+            $logs = LogUser::whereBetween('created_at', [$request['date1'], $request['date2']])
+                                   ->paginate(10)
+                                   ->withQueryString();
+        }
+
+        return view('dashboard.admin.log-user', [
+            'title' => 'Dashboard | Log Users',
+            'logs' => $logs
+        ]);
+    }
 }
